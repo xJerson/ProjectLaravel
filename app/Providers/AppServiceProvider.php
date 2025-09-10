a280905067a4d2075545ba488c50809e0e21602f
JersonRLP <jerson_sukkie@hotmail.com>
2025-09-10 09:14:36 -0500

    Actualiza README con pasos reales de instalación y migración

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema; // 👈 Importante para usar Schema
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 👇 Esta línea soluciona el error de migración
+        Schema::defaultStringLength(191);
     }
 }
